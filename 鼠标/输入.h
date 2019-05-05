6e9747b6293aafa74e79fbce28e6d573fe9abaf1
cflw <cflw@outlook.com>
2019-05-05 20:37:52 +0800

    20190505

---
+++ 鼠标/输入.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include <deque>
+#include <cflw数学.h>
+#include <cflw图形_d2d.h>
+#include <cflw输入_win.h>
+#include <cflw时间.h>
+namespace 数学 = cflw::数学;
+namespace 二维 = cflw::图形::d2d;
+namespace 输入w = cflw::输入::win;
+namespace 时间 = cflw::时间;
+class C输入 {
+public:
+	enum E颜色 {
+		e红,
+		e绿,
+		e蓝,
+	};
+	struct S轨迹点 {
+		数学::S向量2 m坐标;
+		E颜色 m颜色;
+		时间::t时间点 m时间;
+	};
+	void f初始化(HWND);
+	void f销毁();
+	void f计算();
+	void f更新();
+	void f显示();
+	void f消息(UINT, WPARAM, LPARAM);
+	时间::C计帧器 m计帧器0;	//计算
+	时间::C计帧器 m计帧器1;	//渲染
+	二维::C二维 m二维;
+	二维::tp画图形 m画十字;
+	二维::tp画图形 m画点;
+	二维::tp画文本 m画文本;
+	输入w::C输入 m输入;
+	输入w::tp鼠标 m鼠标;
+	std::deque<S轨迹点> ma轨迹点;
+
+};
